2c6b6c3037f5bee2121c0460b700d38b0ec782a8
MinhoJJang <ahchjang@naver.com>
2021-10-30 17:25:38 +0900

    1030_std

--- Basic_Math_2/baek_4948.c
+++ Basic_Math_2/baek_4948.c
@@ -7,7 +7,7 @@
 /*
 예를 들어, 10보다 크고, 20보다 작거나 같은 소수는 4개가 있다. (11, 13, 17, 19) 또, 14보다 크고, 28보다 작거나 같은 소수는 3개가 있다. (17,19, 23)
 
-자연수 n이 주어졌을 때, n보다 크고, 2n보다 작거나 같은 소수의 개수를 구하는 프로그램을 작성하시오. 
+자연수 n이 주어졌을 때, n보다 크고, 2n보다 작거나 같은 소수의 개수를 구하는 프로그램을 작성하시오.
 
 입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 케이스는 n을 포함하는 한 줄로 이루어져 있다.
 
@@ -24,9 +24,9 @@
 로직:
 
 1. 2n의 최댓값만큼의 배열을 만들고, 모두 소수로 가정(TRUE)한다. 물론 1은 제외.
-2. 2를 제외하고, 2의 배수들에게 모두 FALSE를 부여한다. 
-3. 2다음 TRUE인 숫자를 찾고, 그 숫자를 k라고 했을 때 k의 배수들에게 FALSE를 부여한다. 
-4. 그렇게 계속 하다가, k^2 > 2n 이 되는 순간 로직 종료. 
+2. 2를 제외하고, 2의 배수들에게 모두 FALSE를 부여한다.
+3. 2다음 TRUE인 숫자를 찾고, 그 숫자를 k라고 했을 때 k의 배수들에게 FALSE를 부여한다.
+4. 그렇게 계속 하다가, k^2 > 2n 이 되는 순간 로직 종료.
 */
 
 #define PRIME 0
@@ -34,81 +34,85 @@
 #define MAX 246920
 
 
-int arr[MAX] = {PRIME}; // 전부 PRIME 이라고 가정 
-int num[MAX] = {0};
+int arr[MAX] = { PRIME }; // 전부 PRIME 이라고 가정 
+int num[MAX] = { 0 };
 int numIdx = 0;
 
-int answer[MAX] = {0};
+int answer[MAX] = { 0 };
 int ansIdx = 0;
 
-void putData(){
-    do{
-        scanf("%d", &num[numIdx]); 
-        numIdx++;
-    } while (num[numIdx-1] != 0);
+void putData() {
+	while (1) {
+		scanf("%d", &num[numIdx]);
+		if (num[numIdx] == 0) {
+			break;
+		}
+		numIdx++;
+	}
 }
 
-int findNextPrime(int a){
-    int primeIdx = a+1;
-    while(num[primeIdx] != PRIME){
-        primeIdx++;
-    }
-    return primeIdx;
+int findNextPrime(int a) {
+	int primeIdx = a + 1;
+	while (arr[primeIdx] != PRIME) {
+		primeIdx++;
+	}
+	return primeIdx;
 }
 
-void mainLogic(){
+void mainLogic() {
 
-    // 0, 1은 소수가 아님
-    // 1-based 배열
-    arr[0] = NOTPRIME;
-    arr[1] = NOTPRIME;
+	// 0, 1은 소수가 아님
+	// 1-based 배열
+	arr[0] = NOTPRIME;
+	arr[1] = NOTPRIME;
 
-    // 가장 마지막으로 조사한 수 
-    // 일단 위에서 1까지 조사했으니까
-    int currentPrime = findNextPrime(1);
-    //int flag = pow(currentPrime, 2);
-    // 체 거르기
-    while(currentPrime <= (int)sqrt(MAX)){
-        for(int i = 2; i*currentPrime<=MAX; i++){
-            arr[i*currentPrime] = NOTPRIME;
-        }
-        currentPrime = findNextPrime(currentPrime);
-    }
+	// 가장 마지막으로 조사한 수 
+	// 일단 위에서 1까지 조사했으니까
+	int currentPrime = findNextPrime(1);
+	//int flag = pow(currentPrime, 2);
+	// 체 거르기
+	int Maxprime = (int)sqrt(MAX);
+	while (currentPrime <= Maxprime) {
+		for (int i = currentPrime; i*currentPrime <= MAX; i++) {
+			arr[i*currentPrime] = NOTPRIME;
+		}
+		currentPrime = findNextPrime(currentPrime);
+	}
 }
 
-void searchAllPrime(){
+void searchAllPrime() {
 
-    int count = 0;
-    int idx = 0;
+	int idx = 0;
 
-    while(idx<numIdx){
+	while (idx < numIdx) {
 
-        int n = num[idx];
+		int n = num[idx];
+		int count = 0; // 첫번째 실수. count값을 while문 마다 초기화해주어야 하는데 그렇게하지않았음. 
 
-        for(int i=n+1; i<=n*2; i++){
-            if(arr[i]==PRIME){
-                count++;
-            }
-        }
+		for (int i = n + 1; i <= n * 2; i++) {
+			if (arr[i] == PRIME) {
+				count++;
+			}
+		}
 
-        answer[ansIdx] = count;
-        ansIdx++;
-        idx++;
-    }
+		answer[ansIdx] = count;
+		ansIdx++;
+		idx++;
+	}
 }
 
-void printAnswer(){
-    for(int i=0; i<numIdx; i++){
-        printf("%d\n", answer[i]);
-    }
+void printAnswer() {
+	for (int i = 0; i < numIdx; i++) {
+		printf("%d\n", answer[i]);
+	}
 }
 
-int main(){
+int main() {
 
-    putData();
-    mainLogic();
-    searchAllPrime();
-    printAnswer();
+	putData();
+	mainLogic();
+	searchAllPrime();
+	printAnswer();
 
-    return 0;
-}
+	return 0;
+} // 맞았습니다!
