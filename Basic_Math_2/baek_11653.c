27fc00e5871fde7dd64951ddd7a57b68e003a0d1
MinhoJJang <ahchjang@naver.com>
2021-10-31 14:43:36 +0900

    1031study

--- Basic_Math_2/baek_11653.c
+++ Basic_Math_2/baek_11653.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-/* 
+#include <math.h>
+#pragma warning (disable:4996)
+/*
 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
 
 입력
@@ -13,7 +14,7 @@ N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한
 
 #define PRIME 0
 #define NOTPRIME -1
-#define MAX 3165 // sqrt 10,000,000 = 3162.2776...
+#define MAX 10000005 // sqrt 10,000,000 = 3162.2776...
 
 int n = 0;
 int arr[MAX] = { PRIME }; // 전부 PRIME 이라고 가정 
@@ -30,7 +31,7 @@ int findNextPrime(int a) {
 	return primeIdx;
 }
 
-void mainLogic() {
+void mainLogic(int n) {
 
 	// 0, 1은 소수가 아님
 	// 1-based 배열
@@ -42,35 +43,37 @@ void mainLogic() {
 	int currentPrime = findNextPrime(1);
 
 	// 체 거르기
-	int maxPrime = (int)sqrt(MAX)+1;
+	int maxPrime = sqrt(n);
 	while (currentPrime <= maxPrime) {
-		for (int i = currentPrime; i*currentPrime <= MAX; i++) {
+		for (int i = currentPrime; i*currentPrime <= n; i++) {
+			// 여기서 OutOfBounds 오류 발생.
+			// i*currentPrime의 최댓값에 주의
 			arr[i*currentPrime] = NOTPRIME;
 		}
 		currentPrime = findNextPrime(currentPrime);
 	}
 }
 
-void mainFunction(int input){
-    int currentPrime = 2;
+void mainFunction(int input) {
+	int currentPrime = 2;
 
-    while(input != 1){
-        if(input % currentPrime == 0){
-            input /= currentPrime;
-            printf("%d\n", currentPrime);
-            continue;
-        }
-        currentPrime = findNextPrime(currentPrime);
-    }
+	while (input != 1) {
+		if (input % currentPrime == 0) {
+			input /= currentPrime;
+			printf("%d\n", currentPrime);
+			continue;
+		}
+		currentPrime = findNextPrime(currentPrime);
+	}
 }
 
-int main(){
+int main() {
 
-    putData();
-    mainLogic();
-    mainFunction(n);
+	putData();
+	mainLogic(n);
+	mainFunction(n);
 
-    return 0;
+	return 0;
 }
 
-// Out of Bounds???????????? 
+// 맞았습니다!
